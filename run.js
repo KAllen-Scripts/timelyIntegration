3ab6665f1b9748cb221c90fc262eb410d2ff54d5
Kenny Allen <kenny.allen996@gmail.com>
2024-06-13 16:22:39 +0100

    removed info

--- run.js
+++ run.js
@@ -1,11 +1,3 @@
-// SERVER_URL=https://584a-193-115-255-67.ngrok-free.app
-// GROOVE_ACCESS_TOKEN=7780f5588b37746ac32ece1035b8c2b22428a5ed5ff8139841f311b339fe63de
-// TIMELY_CLIENT_ID=SvhLicmoNUOP4nvJ6uiEyxeuloqH4yN4ItktAQwvL9M
-// TIMELY_CLIENT_SECRET=IfY6-b5E18T10YxgHzW5W0DfX8V-AYbihzDw5EONzP4
-// TIMELY_REFRESH=haxCBlJKqoKfIxS2t1RzZx1QayA8BE6CBAm_Ehk0Aws
-// TIMELY_BEARER=UbYGC9W0xfOKQhq90JO8pO3VxPSxrSoJTO7j7aXm4Nk
-// TIMELY_ACCOUNTID=1092419
-
 const Koa = require('koa');
 const Router = require('@koa/router');
 const bodyParser = require('koa-bodyparser');
